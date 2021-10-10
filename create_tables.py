1d48e273175dd70826a082fcc0a739a30ac76b00
Abdulqudus Ayoola <qudus.ayoola123@gmail.com>
2021-10-10 13:21:02 +0100

    Initial commit

---
+++ create_tables.py
@@ -0,0 +1,86 @@
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+def idif_pension_db():
+    """Creates idif_pension_db database, connects and places cursor on idif_pension_db
+    database.
+    """
+    # connect to default database
+    conn = psycopg2.connect("user=postgres \
+                            host=127.0.0.1 \
+                            port=5432 \
+                            dbname=postgres_db \
+                            password=student")
+    cur = conn.cursor()
+    conn.set_session(autocommit=True)
+
+    # create idif_pension_db database
+    cur.execute("""DROP DATABASE IF EXISTS idif_pension_db""")
+    cur.execute("""CREATE DATABASE idif_pension_db""")
+
+    # close intial connection
+    conn.close()
+    cur.close()
+
+    # connect to idif_pension_db
+    conn = psycopg2.connect("user=postgres \
+                            host=127.0.0.1 \
+                            port=5432 \
+                            dbname=idif_pension_db \
+                            password=student")
+    cur = conn.cursor()
+
+    return conn, cur
+
+
+def create_table(conn, cur):
+    """Creates tables in idif_pension_db database by running all create table queries
+    defined in sql_queries.py
+    :param cur: cursor to the database
+    :param conn: database connection reference
+    """
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def drop_table(conn, cur):
+    """Drops tables in idif_pension_db database by running all drop table queries
+    defined in sql_queries.py
+    :param cur: cursor to the database
+    :param conn: database connection reference
+    """
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    """
+    Driver main function.
+    """
+
+    try:
+        conn, cur = create_idif_pension_db()
+    except Exception as e:
+        print(e)
+
+    try:
+        drop_table(conn, cur)
+        print("Table dropped successfully!")
+    except Exception as e:
+        print(e)
+
+    try:
+        create_table(conn, cur)
+        print("Table created successfully!")
+    except Exception as e:
+        print(e)
+
+    # close connection and cursor
+    conn.close()
+    cur.close()
+
+
+if __name__ == "__main__":
+    main()
